5d20feadd0f38964aa569790e965a71a2bc1158e
hojat-gazestani <HojatGazestani@gmail.com>
2023-05-23 14:16:57 +0330

    change hello to app and add host port to app1 and app2

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -6,11 +6,11 @@ from myproject.context_processors import gunicorn_port
 
 hostname = socket.gethostname()
 
-def hello(request):
-    return HttpResponse(f"Hello form {hostname}, Gunicorn port: { gunicorn_port(request)}")
+def app(request):
+    return HttpResponse(f"Application on {hostname}, Gunicorn port: { gunicorn_port(request)}")
     
 def app1(request):
-    return HttpResponse(f"app1 on {hostname}")
+    return HttpResponse(f"app1 on {hostname}, Gunicorn port: { gunicorn_port(request)}")
     
 def app2(request):
-    return HttpResponse(f"app2 on {hostname}")
+    return HttpResponse(f"app2 on {hostname}, Gunicorn port: { gunicorn_port(request)}")
