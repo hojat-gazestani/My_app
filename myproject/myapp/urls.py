3dd320fa7945c5d6f645febaef754bfc4933f473
hojat-gazestani <HojatGazestani@gmail.com>
2023-05-23 14:18:19 +0330

    Add app to root URL in My app

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello, name='hello'),
+    path('', views.app, name='app'),
+    path('app/', views.app, name='app'),
     path('app1/', views.app1, name='app1'),
     path('app2/', views.app2, name='app'),
 ]
