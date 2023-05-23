87dc042684d445aa911902c73fc4c3b58aed2267
hojat-gazestani <HojatGazestani@gmail.com>
2023-05-23 14:28:45 +0330

    Change urls

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.app, name='app'),
-    path('app/', views.app, name='app'),
-    path('app1/', views.app1, name='app1'),
-    path('app2/', views.app2, name='app'),
+    path('app', views.app, name='app'),
+    path('app1', views.app1, name='app1'),
+    path('app2', views.app2, name='app'),
 ]
